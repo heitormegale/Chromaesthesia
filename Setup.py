d2a47d9f4b0c7079232b4e0bff65008c3021d6a7
heitormegale <50935524+heitormegale@users.noreply.github.com>
2019-05-22 20:57:06 -0700

    Add files via upload
    Creates folders that will be used

---
+++ Setup.py
@@ -0,0 +1,6 @@
+#SetUp
+import CreateFolder as createfolder
+
+createfolder.createFolder('\\FinalSong\\')
+createfolder.createFolder('\\FinalFolder\\')
+createfolder.createFolder('\\Separate_Sounds\\')
