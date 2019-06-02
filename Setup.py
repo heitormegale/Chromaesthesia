e8613fd720d65e9f60952888621644dd21e7ec9e
heitormegale <50935524+heitormegale@users.noreply.github.com>
2019-06-02 13:43:53 -0700

    Setup script
    This piece of code will create the necessary folders that will store the files generated and used run the remainder of the project.

--- Setup.py
+++ Setup.py
@@ -1,6 +1,13 @@
-#SetUp
-import CreateFolder as createfolder
+import os
+directory="C:\\Users\\heito\\Desktop\\UCSB\\Spring 2019\\15C\\Music" #<-- your directory
+os.chdir(directory)
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
 
-createfolder.createFolder('\\FinalSong\\')
-createfolder.createFolder('\\FinalFolder\\')
-createfolder.createFolder('\\Separate_Sounds\\')
+createFolder(os.path.join(directory,'IndividualSounds'))
+createFolder(os.path.join(directory,'ImageFolder'))
+createFolder(os.path.join(directory,'Separate_Sounds'))
