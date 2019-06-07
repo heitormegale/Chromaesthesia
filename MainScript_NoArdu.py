5b8b3aee39d599b8d23c03417f084de278a71d6b
heitormegale <50935524+heitormegale@users.noreply.github.com>
2019-06-07 09:46:44 -0700

    Main Script without communication with the scanner
    This script can be used to take an image from a web camera and could be modified to access a folder of images instead of taking a picture. However, it will only produce one tone (three notes) per image.

---
+++ MainScript_NoArdu.py
@@ -0,0 +1,346 @@
+import os
+#os.chdir(r'C:\Users\Switthaus\Documents\UCSB\PhysicsCS15\FinalFolder')
+import sys 
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QColorDialog, QHBoxLayout
+from PyQt5 import QtCore, QtGui
+import time
+import numpy as np
+import seaborn as sns
+import webcolors
+import cv2
+import ImageAlgorithm as imalgo
+import numpy as np
+import matplotlib.pyplot as plt
+import FINALAUDIO as rlrlsound
+import time
+import Audiofinal as adfin
+
+import glob
+#import AudioFinalcompl as adfincmplx
+
+#video = cv2.VideoCapture(0)
+directory = r'C:\Users\heito\Desktop\UCSB\Spring 2019\15C\Music'
+
+class Window(QWidget):
+    def __init__(self, pressed, directory):
+        super().__init__()
+        self.pressed = pressed
+        self.directory = directory
+        self.color = QtCore.Qt.white
+        self.rgb = [255, 0, 0]
+        w = QWidget()
+        
+        MAlgo = QPushButton("Math Algorithm", self)
+        #Algo.move(150, 50)
+        MExpo = QPushButton('Fitted Algorithm Expo', self)
+        #MExpo.move(150, 250)
+        MLine = QPushButton('Fitted Algorithm Linear', self)
+        #MLine.move(150, 450)
+        ColorChoice = QPushButton("Set 0 color", self)
+        
+        vbox = QVBoxLayout()
+        
+        vbox.addWidget(MAlgo)
+        vbox.addWidget(MExpo)
+        vbox.addWidget(MLine)
+        
+        hbox = QHBoxLayout()
+        
+        hbox.addLayout(vbox)
+        hbox.addWidget(ColorChoice)
+        
+        w.setLayout(hbox)
+        
+        MAlgo.clicked.connect(self.clickedMAlgo)
+        MExpo.clicked.connect(self.clickedMExpo)
+        MLine.clicked.connect(self.clickedMLine)
+        ColorChoice.clicked.connect(self.color_picker)        
+        
+        w.show()
+        sys.exit(app.exec())
+        
+    def clickedMAlgo(self):
+        Sound(1, self.directory, self.rgb)
+        
+    def clickedMExpo(self):
+        Sound(2, self.directory, self.rgb)
+          
+    def clickedMLine(self):
+        Sound(3, self.directory, self.rgb)
+        
+    def color_picker(self):
+        print(self.color)
+        col = QColorDialog.getColor(self.color, self)
+        
+        if col.isValid():
+            rgb = (col.red(), col.green(), col.blue())
+            self.setStyleSheet("QWidget { background-color: rgb(%d,%d,%d) }" % rgb)
+            self.rgb = rgb
+            self.color = col
+
+    
+class Sound():
+    def __init__(self, h, directory, rgb):
+        self.h = h
+        self.q = np.array(rgb)
+        self.qHSV = self.RGB_HSV(self.q)
+        self.run(directory)
+                                        
+                                        
+    def RGB_HSV(self, x):
+        x = np.array(x)
+        #print("color in RGB...")
+        #print(x)
+        x = x/255
+        Cmax = max(x)
+        Cmin = min(x)
+        d = Cmax-Cmin
+        if d == 0:
+            H = 0
+        if Cmax == x[0]:
+            H = 60*((x[1]-x[2])/d % 6)
+        if Cmax == x[1]:
+            H = 60*((x[2]-x[0])/d + 2)
+        if Cmax == x[2]:
+            H = 60*((x[0]-x[1])/d + 4)
+
+        if Cmax == 0:
+            S = 0
+        else:
+            S = d/Cmax
+
+        V = Cmax
+    
+        return np.array([H, S, V])
+
+    def sound(self, color):
+    
+        #if (color[0]-self.qHSV[0]) < 0:
+        #    color[0] = 360-color[0]+self.qHSV[0]
+        #else:    
+        #    color[0] = color[0]-self.qHSV[0]
+        if color[0]>340:
+            color[0]=0
+            
+        exp1 = 36*color[0]/360
+        f = (2**(exp1/12))*196
+    
+        if (color[1]-self.qHSV[1]) < 0:
+            color[1] = 1-color[1]+self.qHSV[1]
+        else:
+            color[1]=color[1]-self.qHSV[1]
+  
+        stretch = color[1]*20+3
+        #print(stretch)
+
+        if (color[2]-self.qHSV[2]) < 0:
+            color[2] = 1-color[2]+self.qHSV[2]
+        else:
+            color[2]=color[2]-self.qHSV[2]
+
+        volume = color[2]/5+0.2
+    
+        return [f, stretch, volume]
+    
+
+    def sound_tuned(self, color):
+        #Hue 0
+        if (color[0]-self.qHSV[0]) < 0:
+            color[0] = 360-color[0]+self.qHSV[0]
+        else:    
+            color[0] = color[0]-self.qHSV[0] 
+        if color[0]>340:
+            color[0]=0
+                  
+        #Daniele Sound
+        if color[2] >=0:
+       
+            if color[1] <= 0.15:
+           
+                if color[2] <= 0.85:
+                    color[0] = 220*(2**(color[0]/360))
+                    print(color[0])
+                    
+                else:
+                    color[0] = 880*(2**(color[0]/360)) 
+                    print(color[0])
+               
+            else:
+           
+                if color[2] <= 0.25:
+                    color[0] = 110*(2**(color[0]/360))
+                    print(color[0])
+               
+                else:
+               
+                    if color[2] <= 0.5:
+                        color[0] = 220*(2**(color[0]/360))
+                        print(color[0])
+                   
+                    else:
+                        color[0] = 440*(2**(color[0]/360))
+                        print(color[0]) 
+                        
+        #Stretch                  
+        if (color[1]-self.qHSV[1]) < 0:
+            color[1] = 1-color[1]+self.qHSV[1]
+        else:
+            color[1]=color[1]-self.qHSV[1]
+  
+        color[1] = color[1]*20+3
+
+        #Volume
+        if (color[2]-self.qHSV[2]) < 0:
+            color[2] = 1-color[2]+self.qHSV[2]
+        else:
+            color[2]=color[2]-self.qHSV[2]
+
+        color[2] = color[2]/5+0.2        
+
+        return color    
+    
+    def sound_linear(self, color):
+        if color[2] <= 0.25:
+            color[0] = 0.307*color[0]+110
+            print(color[0])
+        else:
+            if color[2] <= 0.5:
+               color[0] = 0.603995*color[0]+220
+               print(color[0])
+            else:
+                color[0] = 1.251*color[0]+440
+                print(color[0])                    
+        
+                #Stretch                  
+        if (color[1]-self.qHSV[1]) < 0:
+            color[1] = 1-color[1]+self.qHSV[1]
+        else:
+            color[1]=color[1]-self.qHSV[1]
+  
+        color[1] = color[1]*20+3
+
+        #Volume
+        if (color[2]-self.qHSV[2]) < 0:
+            color[2] = 1-color[2]+self.qHSV[2]
+        else:
+            color[2]=color[2]-self.qHSV[2]
+
+        color[2] = color[2]/5+0.2        
+
+        return color                      
+                                                                    
+                                                    
+    def run(self, directory):
+        
+        self.video = cv2.VideoCapture(1)
+        t_begin = time.perf_counter()
+
+        Final_song=os.path.join(directory,'IndividualSounds')
+        Final_folder=os.path.join(directory,'ImageFolder')
+        #files = glob.glob(r"C:\Users\heito\Desktop\UCSB\Spring 2019\15C\Music\FinalSong\*")
+        files = glob.glob(os.path.join(Final_song,"*"))
+        
+        for f in files:
+            os.remove(f)
+
+
+    
+        start=input("Type 1 to start program:")
+        start_1=float(start)
+        
+        #print(ser.read())
+        #while ser.read()==1: #wating for arduino to signal second limit switch
+        #   ser.read()
+
+        #x = b'\x01'
+        ii=0
+        Images=1
+        l = []    
+        ok=1 
+        while ok==1:   
+        
+        
+                while ii<Images :
+                    ii=ii+1
+                    t_start = time.perf_counter()
+                    check, frame = self.video.read()
+                    height, width, channels = frame.shape
+                    frame = frame[int(1/8*(height)):int(7/8*(height)), int(1/5*(width)):int(4/5*(width))]
+    
+                    cv2.imwrite(os.path.join(Final_folder,"image" + str(k) + ".jpg"), frame)
+        
+    
+                    
+            
+    
+    
+                    os.chdir(Final_folder)
+    
+    
+                    b = imalgo.RGBcolors('image' + str(k) + '.jpg')
+                    
+        
+                    #x = np.split(b[1], 3)
+                    t_start = time.perf_counter()
+                    height, width, channels = frame.shape
+    
+                    color1 = b[0]
+                    color2 = b[1]
+                    color3 = b[2]
+            
+                    print(color1)
+
+                    x = webcolors.rgb_to_hex(np.array(color1).astype(int))
+                    y = webcolors.rgb_to_hex(np.array(color2).astype(int))
+                    z = webcolors.rgb_to_hex(np.array(color3).astype(int))
+                      
+                    flatui = [x, y, z]
+                    sns.palplot(sns.color_palette(flatui))
+                    plt.show()
+            
+    
+                    color1HSI = self.RGB_HSV(color1)
+                    color2HSI = self.RGB_HSV(color2)
+                    color3HSI = self.RGB_HSV(color3)
+                    print(color1HSI)
+                    print(color2HSI)
+                    print(color3HSI)
+            
+                    l.append(np.array(color1HSI))
+                    l.append(np.array(color2HSI))
+                    l.append(np.array(color3HSI))
+                    
+                    if self.h == 1:
+                        color1_edited = self.sound(color1HSI)
+                        color2_edited = self.sound(color2HSI)
+                        color3_edited = self.sound(color3HSI)
+                        
+                    if self.h == 2:
+                        color1_edited = self.sound_tuned(color1HSI)
+                        color2_edited = self.sound_tuned(color2HSI)
+                        color3_edited = self.sound_tuned(color3HSI)
+                    
+                    if self.h == 3:
+                        color1_edited = self.sound_tuned(color1HSI)
+                        color2_edited = self.sound_tuned(color2HSI)
+                        color3_edited = self.sound_tuned(color3HSI)   
+                                
+                    rlrlsound.sound(color1_edited, color2_edited, color3_edited, k+1, directory) ##################################
+                    plt.close()
+                    k = k+1
+            
+                    print(time.perf_counter()-t_start)
+                    
+                   
+    
+        
+        adfin.Finalsong(directory)
+        #adfincompl.sound(l)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    pressed = [0, 0, 0]
+    w = Window(pressed, directory)
+    #w.resize(500, 500)
+    #w.show()
+    #sys.exit(app.exec())
